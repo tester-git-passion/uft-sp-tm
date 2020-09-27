a1aa9f5419b9866bde7d3afe28b011e86ebdaf76
roshan jha <roshankumar.miet@gmail.com>
2020-09-27 14:01:17 +0530

    pudh

--- scripts/verifyLogin/Action1/Script.mts
+++ scripts/verifyLogin/Action1/Script.mts
@@ -1,6 +1,7 @@
 ﻿Call Login()
 
 'Verify username
+Set W = Browser("name:=STORE").Page("title:=STORE")
 x = W.WebElement("xpath:=//a[@id='nameofuser']").GetROProperty("name")
 
 If Instr(x,userName) > 0 Then
---
+++ scripts/verifyLogin/Res3/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\pc\\Documents\\UFT Automation\\uft-sp-tm\\scripts\\verifyLogin\\Res3',namePairs:[]}]};
